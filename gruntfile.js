bd6feac2c4f81a16f2e015c445b5756894778eb6
Michael <michal.slabik3@wp.pl>
2017-04-26 14:32:42 +0200

    Change gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
   		},
   		dist: {
   			files: {
-  				'main.css': 'main.sass'
+  				'style.css': 'style.sass'
   			}
   		}
   	},
@@ -38,6 +38,7 @@ module.exports = function(grunt) {
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   
